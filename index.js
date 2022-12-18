743ed102b125286dc35521e8e2771f8bc14260a2
mizbauddintareqe <mizbauddintareq@gmail.com>
2022-12-18 14:36:16 +0600

    added single service details

--- index.js
+++ index.js
@@ -8,17 +8,41 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.taxrqnn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-console.log(uri);
+
 async function run() {
   try {
-    // const collection = client.db("test").collection("devices");
+    const servicesCollection = client
+      .db("geoWild-photography")
+      .collection("services");
+
+    //   Get limited services API
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query);
+      const services = await cursor.limit(3).toArray();
+      res.send(services);
+    });
+
+    //   Get all service API
+    app.get("/services/all", async (req, res) => {
+      const allServices = await servicesCollection.find({}).toArray();
+      res.send(allServices);
+    });
+
+    //   Get single service by id API
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await servicesCollection.findOne(query);
+      res.send(service);
+    });
   } finally {
   }
 }
