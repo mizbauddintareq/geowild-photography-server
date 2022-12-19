2763d32806737aff779d68293883f8f6458247b7
mizbauddintareqe <mizbauddintareq@gmail.com>
2022-12-20 02:40:35 +0600

    complete crud

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
@@ -8,7 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.taxrqnn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -69,6 +69,14 @@ async function run() {
       res.send(reviews);
     });
 
+    // Get review by id API
+    app.get("/review/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const review = await reviewsCollection.findOne(query);
+      res.send(review);
+    });
+
     // Get my reviews API
     app.get("/myReviews", async (req, res) => {
       const email = req.query.email;
@@ -81,6 +89,35 @@ async function run() {
       const cursor = reviewsCollection.find(query);
       const myReviews = await cursor.toArray();
       res.send(myReviews);
+    });
+
+    // Delete review API
+    app.delete("/review/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.put("/review/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const review = req.body;
+      const option = { upsert: true };
+      const updateDoc = {
+        $set: {
+          userName: review.userName,
+          userEmail: review.userEmail,
+          serviceName: review.serviceName,
+        },
+      };
+
+      const result = await reviewsCollection.updateOne(
+        filter,
+        updateDoc,
+        option
+      );
+      res.send(result);
     });
   } finally {
   }
